ff6b80093947210ad1b5417db6176dbbd30f9061
Eitan Fine <33404825+EitanFine@users.noreply.github.com>
2019-05-02 21:11:36 -0400

    Create validParens.js

---
+++ validParens.js
@@ -0,0 +1,22 @@
+const balancedBrackets = sequence => {
+  if (sequence.length % 2 !== 0) return false;
+
+  let arrSeq = ["[", "]", "(", ")", "{", "}"];
+  let stack = [];
+  let val;
+
+  for (let i = 0; i < sequence.length; i++) {
+    val = arrSeq.indexOf(sequence[i]);
+    // console.log("val", val);
+    if (Math.abs(val - stack[stack.length - 1]) === 1) {
+      stack.pop();
+    } else {
+      stack.push(val);
+    }
+    // console.log(stack);
+  }
+
+  return stack.length === 0 ? true : false;
+};
+
+console.log(balancedBrackets("[({{}()})[]]"));
