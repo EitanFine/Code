e4affd1d845ecc7eca0ed16a551c4ec98a097ae7
Eitan Fine <33404825+EitanFine@users.noreply.github.com>
2019-05-02 21:28:13 -0400

    Update validParens.js

--- validParens.js
+++ validParens.js
@@ -1,3 +1,4 @@
+//Solution 1: Arrays
 const balancedBrackets = sequence => {
   if (sequence.length % 2 !== 0) return false;
 
@@ -7,6 +8,30 @@ const balancedBrackets = sequence => {
 
   for (let i = 0; i < sequence.length; i++) {
     val = arrSeq.indexOf(sequence[i]);
+    // console.log("val", val);
+    if (Math.abs(val - stack[stack.length - 1]) === 1) {
+      stack.pop();
+    } else {
+      stack.push(val);
+    }
+    // console.log(stack);
+  }
+
+  return stack.length === 0 ? true : false;
+};
+
+console.log(balancedBrackets("[({{}()})[]]"));
+
+//Solution 2: Maps
+const balancedBrackets = sequence => {
+  if (sequence.length % 2 !== 0) return false;
+
+  let arrSeq = { "[": 0, "]": 1, "(": 2, ")": 3, "{": 4, "}": 5 };
+  let stack = [];
+  let val;
+
+  for (let i = 0; i < sequence.length; i++) {
+    val = arrSeq[sequence[i]];
     // console.log("val", val);
     if (Math.abs(val - stack[stack.length - 1]) === 1) {
       stack.pop();
