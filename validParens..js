47ae96bbd7489dbf4c6cea2f2711f9c71fbd8325
Eitan Fine <33404825+EitanFine@users.noreply.github.com>
2019-05-06 16:19:54 -0400

    Create validParens..js
    Using an Array as a Stack

---
+++ validParens..js
@@ -0,0 +1,22 @@
+// Using an Array as a Stack
+
+const balanced_brackets = str => {
+  let len = str.length;
+  let arr = [];
+  let obj = { "]": "[", ")": "(", "}": "{" };
+
+  if (len % 2 !== 0) return false;
+
+  for (let i = 0; i < len; i++) {
+    if (str[i] === "[" || str[i] === "(" || str[i] === "{") {
+      arr.push(str[i]);
+    } else if (arr[arr.length - 1] !== obj[str[i]]) {
+      return false;
+    } else {
+      arr.pop();
+    }
+  }
+  if (arr.length === 0) return true;
+};
+
+console.log(balanced_brackets("[()]"));
